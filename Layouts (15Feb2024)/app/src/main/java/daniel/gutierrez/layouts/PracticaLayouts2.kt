2f79ad1624eac52ff31dcaa64a8139b07b38c669
Dan Gutiérrez <dany_rocker_96@hotmail.com>
2024-02-20 05:26:16 -0700

    Cambio de Nombre de Archivos y Update

--- Layouts (15Feb2024)/app/src/main/java/daniel/gutierrez/layouts/PracticaLayouts2.kt
+++ Layouts (15Feb2024)/app/src/main/java/daniel/gutierrez/layouts/PracticaLayouts2.kt
@@ -3,9 +3,9 @@ package daniel.gutierrez.layouts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PracticaLayouts2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_practica_layouts2)
     }
 }
